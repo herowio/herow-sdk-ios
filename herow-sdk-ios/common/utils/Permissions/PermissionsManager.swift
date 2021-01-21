8c26b3461adee1ae5742954e488bcf6ce8743fec
damien <dfe@connecthings.com>
2021-01-21 14:48:18 +0100

    Work on userInfoManager

--- herow-sdk-ios/common/utils/Permissions/PermissionsManager.swift
+++ herow-sdk-ios/common/utils/Permissions/PermissionsManager.swift
@@ -1,5 +1,5 @@
 //
-//  PermissionManager.swift
+//  PermissionsManager.swift
 //  herow-sdk-ios
 //
 //  Created by Damien on 21/01/2021.
@@ -7,7 +7,24 @@
 
 import Foundation
 import CoreLocation
+import AppTrackingTransparency
+@objc class PermissionsManager: NSObject  {
+    let userInfoManager: UserInfoManagerProtocol
+    let dataHolder: DataHolder
 
-class PermissionManager {
+     init(userInfoManager: UserInfoManagerProtocol, dataHolder: DataHolder ) {
+        self.userInfoManager = userInfoManager
+        self.dataHolder = dataHolder
+    }
+
+    func requestIDFA(completion: (()->())? = nil) {
+        if #available(iOS 14, *) {
+            ATTrackingManager.requestTrackingAuthorization(completionHandler: { status in
+
+             })
+        } else {
+            // Fallback on earlier versions
+        }
+    }
 
 }
--- herow-sdk-ios/connexion/userInfo/UserInfoManager.swift
+++ herow-sdk-ios/connexion/userInfo/UserInfoManager.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+class UserInfoManager: UserInfoManagerProtocol {
+    static let customIdKey: String = "com.herow.sdk.user.customid"
+    static let pushIdKey: String = "com.herow.sdk.user.pushid"
+    static let synchroKey: String = "com.herow.sdk.user.synchro"
+    static let idfvKey: String = "com.herow.sdk.user.idfv"
+    static let idfaKey: String = "com.herow.sdk.user.idfa"
+    static let herowIdKey: String = "com.herow.sdk.user.herowid"
+    static let langKey: String = "com.herow.sdk.user.lang"
+    static let utcOffsetKey: String = "com.herow.sdk.user.utc.offset"
+    static let herowIdStatusKey: String = "com.herow.sdk.user.herowid.status"
+    static let locationStatusKey: String = "com.herow.sdk.user.location.status"
+    static let accuracyStatusKey: String = "com.herow.sdk.user.accuracy.status"
+    static let notificationStatusKey: String = "com.herow.sdk.user.notification.status"
+}
